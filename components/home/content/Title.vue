ea0bb20f2f69d2330c4f5e2089a728edeb3fa845
Paul Haddou <32875065+PaulHaddou@users.noreply.github.com>
2022-11-15 15:12:07 +0100

    Home - content - title : component created

---
+++ components/home/content/Title.vue
@@ -0,0 +1,45 @@
+<template>
+  <h3
+    class="title"
+    :class="'title--' + projectKey"
+  >
+    {{ projectTitle }}
+    <span
+      class="title__underline"
+      :class="'title--underline--' + projectKey"
+    />
+  </h3>
+</template>
+<script>
+export default {
+  props: {
+    projectTitle: {
+      type: String,
+      default: ''
+    },
+    projectKey: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+<style
+  lang="scss"
+  scoped
+>
+  .title {
+    font-size: 60px;
+    margin: 0;
+    width: fit-content;
+    position: relative;
+    &__underline {
+      width: calc(100% - 4px + 24px);
+      height: 22px;
+      position: absolute;
+      bottom: 8px;
+      left: 0;
+      z-index: -10;
+    }
+  }
+</style>
